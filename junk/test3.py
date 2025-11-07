e52b82dc5fae593a97cacd38b2ad224ac3e8b86c
nujufas <nujufas@gmail.com>
2025-11-07 12:06:09 +0100

    temp

--- junk/test3.py
+++ junk/test3.py
@@ -1,6 +1,6 @@
 import numpy as np
 # from scipy.spatial.transform import Rotation as R
-from transformation import create_transformation_matrix
+from transformation import Transformation
 
 # def create_transformation_matrix(coordinates, angles):
 #     rotation = R.from_euler('zyx', angles, degrees=False)
@@ -10,19 +10,21 @@ from transformation import create_transformation_matrix
 #     T[:3, :3] = rot_matrix
 #     T[:3, 3] = coordinates
 #     return T
+transformer = Transformation([1.828963, -138.407091, -0.006], [1.571584, -0.004266, -0.000046])
+# ego_translation = [1.828963, -138.407091, -0.006]
+# angles = [1.571584, -0.004266, -0.000046]
+# T = create_transformation_matrix(ego_translation, angles)
 
-ego_translation = [1.828963, -138.407091, -0.006]
-angles = [1.571584, -0.004266, -0.000046]
-T = create_transformation_matrix(ego_translation, angles)
-
-print("Homogeneous Transformation Matrix:")
-print(T)
+# print("Homogeneous Transformation Matrix:")
+print(transformer.T)
+print(transformer.T_inv)
 
 # Another object's coordinates in ego frame (as homogeneous vector)
 # object_coords = np.array([3, 4, 1, 1])  # [x, y, z, 1]
 object_coords = np.array([4.985880, 112.518, 0.0, 1])
+# object_coords = [4.985880, 112.518, 0.0, 1]
 
 # Transform to world frame
-world_coords = T @ object_coords
+world_coords = transformer.apply_transformation(object_coords)
 print("\nObject coordinates in world frame:")
 print(world_coords[:3])
--- src/transformation.py
+++ src/transformation.py
@@ -1,12 +1,22 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-def create_transformation_matrix(coordinates, angles):
-    rotation = R.from_euler('zyx', angles, degrees=False)
-    rot_matrix = rotation.as_matrix()
+class Transformation:
+    def __init__(self, coordinates, angles):
+        self.coordinates = coordinates
+        self.angles = angles
+        self.T, self.T_inv = Transformation.create_transformation_matrix(coordinates, angles)
 
-    T = np.eye(4)
-    T[:3, :3] = rot_matrix
-    T[:3, 3] = coordinates
-    return T
+    @staticmethod
+    def create_transformation_matrix(coordinates, angles):
+        rotation = R.from_euler('zyx', angles, degrees=False)
+        rot_matrix = rotation.as_matrix()
+        T = np.eye(4)
+        T[:3, :3] = rot_matrix
+        T[:3, 3] = coordinates
+        T_inv = np.linalg.inv(T)
+        return T, T_inv
 
+    def apply_transformation(self, object_coords):
+        transformed_points = self.T_inv @ object_coords
+        return transformed_points
